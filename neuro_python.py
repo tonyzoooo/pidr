9eac1ed4e25a197efa0f5ee99dc01b2a3756a888
Loïc Bertrand <loic.bertrand@telecomnancy.eu>
2020-01-25 15:02:45 +0100

    Avancement de la traduction

--- neuro_python.py
+++ neuro_python.py
@@ -3,7 +3,7 @@ from numpy.linalg import norm
 
 
 def morphofiltd(re, ordre, r0, r1, rN, rD=None, Cs=1):
-    """inputs: 
+    """inputs:
         elpos = electrode positions (M x 3)
         ordre = filter length (N+1 = nb of compartments on the axon + soma)
         r0 = soma position (1 x 3) (center)
@@ -229,7 +229,7 @@ Imlfpy=dlmread(['../Python/Im_BS_LA',num2str(LA),'_DA',num2str(DA),'_LD',num2str
 %% figure check
 figure
 subplot(2,1,1)
-plot(Vm([inMVm-inmvm+1:inMVm-inmvm+lVLFPy])) 
+plot(Vm([inMVm-inmvm+1:inMVm-inmvm+lVLFPy]))
 hold on
 plot(Vmlfpy)
 
@@ -238,62 +238,73 @@ plot(Im([inMVm-inmvm+1:inMVm-inmvm+lVLFPy]))
 hold on
 plot(Imlfpy)
 
+"""
+# filter parameters
+dk = 10
+# axonal spatial sampling(~ nb of segments)
+ordre = LA/dk+1
+r0 = array([0, 0, 0])
+# soma position
+r1 = array([SL/2, 0, 0])
+# axon start position
+rN = array([SL/2+LA-dk, 0, 0])
+# axon stop position(start of the last segment)
+rd = norm(r1-r0) * array([sin(phi)*cos(theta), sin(phi)*sin(theta), cos(phi)])
+# dendrite end position, normalized
+Cs = 2
+# somatic equivalent dipole amplitude
+taus = 23
+# subsampling of the membrane current dk/taus = speed v)
 
-%% filter parameters
-dk=10; % axonal spatial sampling (~ nb of segments)
-ordre=LA/dk+1;
-r0=[0 0 0]; % soma position
-r1=[SL/2 0 0]; % axon start position
-rN=[SL/2+LA-dk 0 0]; % axon stop position (start of the last segment)
-rd=norm(r1-r0)*[sin(phi)*cos(theta) sin(phi)*sin(theta) cos(phi)]; % dendrite end position, normalized
-Cs=2; % somatic equivalent dipole amplitude
-taus=23; % subsampling of the membrane current dk/taus = speed v)
+# electrodes
+X = arange(-250, 1250+125, 125).H
+Y = arange(250, 50-50, -50).H
+Z = 0
 
-%% electrodes
-X=[-250:125:1250]';
-Y=[250:-50:50]';
-Z=0;
+"""
+eplosy, elposx, elposz = meshgrid(Y, X, Z)
+elpos = [elposx(:), eplosy(:), elposz(:)]
 
-[eplosy,elposx,elposz]=meshgrid(Y,X,Z);
-elpos=[elposx(:),eplosy(:),elposz(:)]
+# simulation
+w = morphofiltd(elpos, ordre, r0, r1, rN, rd, Cs)
+wup = upsample(w',taus)'
 
-%% simulation
-w = morphofiltd(elpos,ordre,r0,r1,rN,rd,Cs);
-wup=upsample(w',taus)';
-
-Vel=zeros(size(w,1),length(Im));
-for iel=1:size(w,1),
-    Vel(iel,:)=conv(Im,wup(iel,:),'same');
+Vel=zeros(size(w, 1), length(Im))
+for iel=1:
+    size(w, 1),
+    Vel(iel, :) = conv(Im, wup(iel, : ), 'same')
 end
-% cut
-intervVm=[inMVm-inmvm-fix(size(wup,2)/2)+1:inMVm-inmvm-fix(size(wup,2)/2)+lVLFPy];
-Vel2=Vel(:,intervVm);
-% normalize
-elsync=56;
-Vel2=Vel2/norm(Vel2(elsync,:))*norm(Vlfpy(:,elsync));
-%% plot grid 
+# cut
+intervVm=[inMVm-inmvm-fix(size(wup, 2)/2)+1:inMVm-inmvm-fix(size(wup, 2)/2)+lVLFPy]
+Vel2 = Vel(: , intervVm)
+# normalize
+elsync=56
+Vel2 = Vel2/norm(Vel2(elsync, : ))*norm(Vlfpy(: , elsync))
+# plot grid
 
-cc=zeros(1,size(elpos,1));
-t=dt:dt:dt*size(Vel2,2);
+cc=zeros(1, size(elpos, 1))
+t=dt: dt: dt*size(Vel2, 2)
 figure
-cmap=colormap;
-for ifil=1:size(elpos,1),
-    subplot(5,13,ifil);
-    plot(t,Vel2(ifil,:)-Vel2(ifil,1),'LineWidth',2)
+cmap=colormap
+for ifil=1:
+    size(elpos, 1),
+    subplot(5, 13, ifil)
+    plot(t, Vel2(ifil, :)-Vel2(ifil, 1), 'LineWidth', 2)
     hold on
-    plot(t,Vlfpy(:,ifil)-Vlfpy(1,ifil),'LineWidth',2)
-    cc(ifil)=corr(Vel2(ifil,:)',Vlfpy(:,ifil));
-    scatter(4,-2*10**-3,100,cmap(1+fix(size(cmap,1)*cc(ifil)),:),'filled')
+    plot(t, Vlfpy(:, ifil)-Vlfpy(1, ifil), 'LineWidth', 2)
+    cc(ifil) = corr(Vel2(ifil, :)', Vlfpy(: , ifil))
+    scatter(4, -2*10**-3, 100, cmap(1+fix(size(cmap, 1)*cc(ifil)), : ), 'filled')
     ylim([-5 5]*10**-3)
-    if ifil>52
-        text(2,-12*10**-3,[num2str((ifil-53)*125-250),'\mu','m'])
+    if ifil > 52
+    text(2, -12*10**-3, [num2str((ifil-53)*125-250), '\mu', 'm'])
     end
-    if rem(ifil,13)==1,
-       text(-8,0*10**-3,[num2str(-fix(ifil/13)*50+250),'\mu','m'])
-    end 
+    if rem(ifil, 13) == 1,
+    text(-8, 0*10**-3, [num2str(-fix(ifil/13)*50+250), '\mu', 'm'])
+    end
     axis off
 end
-colorbar('Position',[0.93 0.3 0.007 0.6],'FontSize',14)
+colorbar('Position', [0.93 0.3 0.007 0.6], 'FontSize', 14)
 
-fprintf('\n Mean correlation = %1.2f \n Min correlation = %1.2f  \n Max correlation = %1.2f \n',mean(cc),min(cc),max(cc))
+fprintf('\n Mean correlation = %1.2f \n Min correlation = %1.2f  \n Max correlation = %1.2f \n',
+        mean(cc), min(cc), max(cc))
 """
