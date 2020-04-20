d627b57b303db0c6c3f72ff4973ecc2cfdf93bab
Loïc Bertrand <loic.bertrand@telecomnancy.eu>
2020-04-20 15:28:20 +0200

    Encapsulation en méthodes et namespaces np/plt

--- src/demo.py
+++ src/demo.py
@@ -1,14 +1,10 @@
-from pathlib import Path
-
+import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.image import imread
-from matplotlib.pyplot import (axis, cm, colorbar, colormaps, figure, imshow,
-                               plot, savefig, scatter, show, subplot, subplots,
-                               text, ylim)
-from numpy import (arange, argmax, array, convolve, corrcoef, cos, fix,
-                   heaviside, linspace, max, mean, meshgrid, min, ndarray, pi,
-                   sin, size, zeros)
+from numpy import pi
 from numpy.linalg import norm
 
+from demo_github import getStimulationResult, plotStimulation
 from hhrun import hhrun
 from morphofiltd import morphofiltd
 from util import readMatrix, reshapeMeshgrid, upsample
@@ -22,14 +18,14 @@ lVLFPy = 8000   # signal length in LFPy
 dt = 10**(-3)   # in ms
 Nt = 2**15
 D = Nt*dt
-t = arange(dt, D+dt, dt)-dt
+t = np.arange(dt, D+dt, dt)-dt
 n = len(t)
 
 fe = 1/dt
-f = arange(0, fe/2, fe/Nt)
+f = np.arange(0, fe/2, fe/Nt)
 
 I = (
-    (heaviside(t-1, 1/2)-heaviside(t-31, 1/2))
+    (np.heaviside(t-1, 1/2)-np.heaviside(t-31, 1/2))
     * 0.044 / (2*pi*12.5*25) * 10**8*10**-3
     # * 5.093 # 0.15/(pi*12.5*12.5*2+2*pi*12.5*25)*10**8
 )
@@ -40,7 +36,7 @@ icur = 1
 [Vm, m, n, h, INa, IK, Il] = hhrun(I, t)
 
 Im = (INa+IK+Il) * (2*pi*12.5*25) / 10**8 * 10**3
-inMVm = argmax(Vm)
+inMVm = np.argmax(Vm)
 MVm = Vm[inMVm]
 
 # -----------------------------------------------------------
@@ -61,28 +57,34 @@ theta = pi  # angle with Ox (phi=pi/2,theta=pi) indicates opposite to the axon
 # load LFPy simulation result
 # -----------------------------------------------------------
 
-data = Path('data/')  # os independent path
-Vlfpy = readMatrix(data / f'Vlfpy_BS_LA{LA}_DA{DA}_LD{LD}_DD{DD}demo.txt')
-Vmlfpy = readMatrix(data / f'Vm_BS_LA{LA}_DA{DA}_LD{LD}_DD{DD}demo.txt')
-Imlfpy = readMatrix(data / f'Im_BS_LA{LA}_DA{DA}_LD{LD}_DD{DD}demo.txt')
+# data = Path('data/')  # os independent path
+# Vlfpy = readMatrix(data / f'Vlfpy_BS_LA{LA}_DA{DA}_LD{LD}_DD{DD}demo.txt')
+# Vmlfpy = readMatrix(data / f'Vm_BS_LA{LA}_DA{DA}_LD{LD}_DD{DD}demo.txt')
+# Imlfpy = readMatrix(data / f'Im_BS_LA{LA}_DA{DA}_LD{LD}_DD{DD}demo.txt')
+
+result = getStimulationResult()
+
+Vlfpy = result.Vlfpy
+Vmlfpy = result.Vmlfpy
+Imlfpy = result.Imlfpy
 
 # -----------------------------------------------------------
 # figure check
 # -----------------------------------------------------------
 
-values = arange(inMVm-inmvm, inMVm-inmvm+lVLFPy)
+values = np.arange(inMVm-inmvm, inMVm-inmvm+lVLFPy)
 
-figure()
+plt.figure(3)
 
-subplot(2, 1, 1)
-plot(Vm[values])
-plot(Vmlfpy)
+plt.subplot(2, 1, 1)
+plt.plot(Vm[values])
+plt.plot(Vmlfpy)
 
-subplot(2, 1, 2)
-plot(Im[values])
-plot(Imlfpy)
+plt.subplot(2, 1, 2)
+plt.plot(Im[values])
+plt.plot(Imlfpy)
 
-show()
+plt.show()
 
 # -----------------------------------------------------------
 # filter parameters
@@ -91,13 +93,14 @@ show()
 dk = 10     # axonal spatial sampling(~ nb of segments)
 
 order = int(LA/dk+1)
-r0 = array([0, 0, 0])           # soma position
-r1 = array([SL/2, 0, 0])        # axon start position
-rN = array([SL/2+LA-dk, 0, 0])  # axon stop position (start of the last segment)
-rd = norm(r1-r0) * array([
-    sin(phi) * cos(theta),
-    sin(phi) * sin(theta),
-    cos(phi)
+r0 = np.array([0, 0, 0])           # soma position
+r1 = np.array([SL/2, 0, 0])        # axon start position
+# axon stop position (start of the last segment)
+rN = np.array([SL/2+LA-dk, 0, 0])
+rd = norm(r1-r0) * np.array([
+    np.sin(phi) * np.cos(theta),
+    np.sin(phi) * np.sin(theta),
+    np.cos(phi)
 ])          # dendrite end position, normalized
 Cs = 2      # somatic equivalent dipole amplitude
 taus = 23   # subsampling of the membrane current dk/taus = speed v)
@@ -106,11 +109,11 @@ taus = 23   # subsampling of the membrane current dk/taus = speed v)
 # electrodes
 # -----------------------------------------------------------
 
-X = arange(-250, 1250+125, 125)
-Y = arange(250, 50-50, -50)
+X = np.arange(-250, 1250+125, 125)
+Y = np.arange(250, 50-50, -50)
 Z = 0
 
-elpos = reshapeMeshgrid(meshgrid(X, Y, Z)).transpose()
+elpos = reshapeMeshgrid(np.meshgrid(X, Y, Z)).transpose()
 
 # -----------------------------------------------------------
 # simulation
@@ -119,16 +122,16 @@ elpos = reshapeMeshgrid(meshgrid(X, Y, Z)).transpose()
 w = morphofiltd(elpos, order, r0, r1, rN, rd, Cs)
 wup = upsample(w.transpose(), taus).transpose()
 
-Vel = zeros((len(w), len(Im)))
+Vel = np.zeros((len(w), len(Im)))
 
 for iel in range(len(w)):
-    Vel[iel] = convolve(Im, wup[iel], 'same')
+    Vel[iel] = np.convolve(Im, wup[iel], 'same')
 
 # cut
-commonPart = inMVm - inmvm - int(fix(wup.shape[1]/2))
+commonPart = inMVm - inmvm - int(np.fix(wup.shape[1]/2))
 rangeStart = commonPart + 1
 rangeEnd = commonPart + lVLFPy
-intervVm = arange(rangeStart, rangeEnd + 1)
+intervVm = np.arange(rangeStart, rangeEnd + 1)
 Vel2 = Vel[:, intervVm]
 
 # normalize
@@ -139,27 +142,27 @@ Vel2 = Vel2 / norm(Vel2[elsync, :]) * norm(Vlfpy[:, elsync])
 # plot grid
 # -----------------------------------------------------------
 
-cc = zeros((1, elpos.shape[0]))
-t = arange(0, dt * Vel2.shape[1], dt)
+cc = np.zeros((1, elpos.shape[0]))
+t = np.arange(0, dt * Vel2.shape[1], dt)
 
-fig, ax = subplots()
-cmap = cm.get_cmap('jet')
+fig, ax = plt.subplots()
+cmap = plt.cm.get_cmap('jet')
 
 for ifil in range(elpos.shape[0]):
-    subplot(5, 13, ifil+1)
-    plot(t, Vel2[ifil]-Vel2[ifil, 0], linewidth=2)
-    plot(t, Vlfpy[:, ifil]-Vlfpy[0, ifil], linewidth=2)
-    a = corrcoef(Vel2[ifil].transpose(), Vlfpy[:, ifil])[0][1]
-    cc[0, ifil] = corrcoef(Vel2[ifil].transpose(), Vlfpy[:, ifil])[0][1]
+    plt.subplot(5, 13, ifil+1)
+    plt.plot(t, Vel2[ifil]-Vel2[ifil, 0], linewidth=2)
+    plt.plot(t, Vlfpy[:, ifil]-Vlfpy[0, ifil], linewidth=2)
+    a = np.corrcoef(Vel2[ifil].transpose(), Vlfpy[:, ifil])[0][1]
+    cc[0, ifil] = np.corrcoef(Vel2[ifil].transpose(), Vlfpy[:, ifil])[0][1]
     rgba = cmap(cc[0, ifil])
-    color = array([[rgba[i] for i in range(3)]])
-    scatter(4, -2 * 10**-3, 50, color, 'o', cmap)
-    ylim(array([-5, 5]) * 10**-3)
+    color = np.array([[rgba[i] for i in range(3)]])
+    plt.scatter(4, -2 * 10**-3, 50, color, 'o', cmap)
+    plt.ylim(np.array([-5, 5]) * 10**-3)
     if ifil > 51:
-        text(2, -12*10**-3, str((ifil-53)*125-250+125) + '\u03BCm')
+        plt.text(2, -12*10**-3, str((ifil-53)*125-250+125) + '$\mu$m')
     if ifil % 13 == 0:
-        text(-8, 0*10**-3, str(-fix(ifil/13)*50+250) + '\u03BCm')
-    axis('off')
+        plt.text(-8, 0*10**-3, str(-np.fix(ifil/13)*50+250) + '$\mu$m')
+    plt.axis('off')
 
 fig.savefig('simulation_results.png', bbox_inches='tight')
 im = imread('simulation_results.png')
@@ -168,8 +171,19 @@ im = ax.imshow(im, cmap=cmap)
 fig.colorbar(im, cax=pos, orientation='vertical')
 fig.savefig('simulation_results.png', bbox_inches='tight')
 
-show()
+plt.show()
 
-print('Mean correlation = ' + '{0:.2f}'.format(mean(cc)))
-print('Min correlation = ' + '{0:.2f}'.format(min(cc)))
-print('Max correlation = ' + '{0:.2f}'.format(max(cc)))
+print('Mean correlation = ' + '{0:.2f}'.format(np.mean(cc)))
+print('Min correlation = ' + '{0:.2f}'.format(np.min(cc)))
+print('Max correlation = ' + '{0:.2f}'.format(np.max(cc)))
+
+# -----------------------------------------------------------
+# plot stimulation
+# -----------------------------------------------------------
+
+cell = result.cell
+timeind = result.timeind
+stimulus = result.stimulus
+meshgrid_electrodes = result.meshgrid_electrodes
+
+plotStimulation(cell, timeind, stimulus, meshgrid_electrodes)
---
+++ src/demo_github.py
@@ -0,0 +1,207 @@
+import LFPy
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab as pl
+from matplotlib.collections import PolyCollection
+#import scipy
+#from scipy.signal import butter, lfilter
+#import matplotlib.animation as animation
+from neuron import h
+import os
+import math
+
+
+def PolyArea(x, y):
+    return 0.5*np.abs(np.dot(x, np.roll(y, 1))-np.dot(y, np.roll(x, 1)))
+
+
+def plotstuff(cell, electrode):
+    '''plotting'''
+    fig = plt.figure(2)
+    plt.plot(electrode.x, electrode.y, '.',  marker='o',
+             markersize=3, color='r', zorder=0)
+    #    rotation = {'x' : 0, 'y' : math.pi, 'z' : 0} #-math.pi/9 # Mainen
+    #    cell.set_rotation(**rotation)
+    # Plot neuron morphology
+    zips = []
+    for x, y in cell.get_idx_polygons(projection=('x', 'y')):
+        # PATCH Radu do not plot big boxes
+        #        tmp=list(zip(x,y))
+        #        if not 250 in tmp[1]:
+        if PolyArea(x, y) < 10000:
+            zips.append(list(zip(x, y)))
+        # END PATCH
+    polycol = PolyCollection(zips, edgecolors='k', facecolors='k')
+    ax = fig.add_subplot(111)
+    ax.add_collection(polycol)
+    ax.axis(ax.axis('equal'))
+    plt.xlabel('Distance $\mu$m - (Ox)')
+    plt.ylabel('Distance $\mu$m - (0y) ')
+    plt.grid(True)
+    plt.title(r'$Neuron$ $Morphology$')
+    # plt.show()
+    return fig    
+
+def getStimulationResult():
+    # =============================================================================
+    # ================================= MORPHOLOGY ================================
+    # =============================================================================
+
+    LA = "1000"
+    DA = "2"
+    LD = "50"
+    DD = "2"
+
+    st = 1/1000
+
+    filename = "./BSR_LA"+LA+"_DA"+DA+"_LD"+LD+"_DD"+DD+"_demo.hoc"
+    cell_parameters = {
+        'morphology': filename,
+        'v_init': -65,
+        'passive': True,
+        'passive_parameters': {'g_pas': 1./30000, 'e_pas': -65},
+        'cm': 1.0,
+        'Ra': 150,
+        'dt': st,
+        'tstart': 0.,
+        'tstop': 20.,
+        'nsegs_method': 'lambda_f',  # spatial discretization method
+        'lambda_f': 100.,  # frequency where length constants are computed
+    }
+    cell = LFPy.Cell(**cell_parameters)
+
+
+    # =============================================================================
+    # ================================= stimulation parameters================================
+    # =============================================================================
+
+    amp = 0.2
+    dur = 10
+    delay = 1
+    #                if cell_parameters["passive"]==False:
+    #                    amp=1.95**(int(ida)/1.95)/30+1.6*int(ila)/10000+int(ild)/5000*int(idd)*1.5
+    #                else:
+    #                    amp=1.65**(int(ida)/1.95)/30+0.805*int(ila)/10000+int(ild)/5000*int(idd)*1.1 #-0.4 for Mainen equivalent
+
+    stim = {
+        'idx': cell.get_closest_idx(x=0, y=0, z=0),
+        'record_current': True,
+        'pptype': 'IClamp',
+        'amp': amp,
+        'dur': dur,  # 0.01
+        'delay': delay,  # 5
+    }
+
+
+    stimulus = LFPy.StimIntElectrode(cell, **stim)
+
+
+    # =============================================================================
+    # ================================= SIMULATION  ===============================
+    # =============================================================================
+
+
+    cell.simulate(rec_imem=True)
+    # cell.imem[np.isnan(cell.imem)]=0.0
+
+
+    # =============================================================================
+    # ================================= electrodes ================================
+    # =============================================================================
+
+    hstep = np.array(range(-250, 1251, 125))
+    vstep = np.array(range(250, 49, -50))
+    N = vstep.shape
+    Ny = hstep.shape
+
+    x_elec = np.tile(hstep, N)
+    y_elec = np.sort(np.tile(vstep, Ny))[::-1]
+    z_elec = np.zeros(x_elec.shape)
+
+
+    meshgrid = {
+        'sigma': 0.33,
+        'x': x_elec,
+        'y': y_elec,
+        'z': z_elec,
+    }
+
+    # Electrodes initialization
+    meshgrid_electrodes = LFPy.RecExtElectrode(cell, **meshgrid)
+    meshgrid_electrodes.calc_lfp()
+
+
+    # =============================================================================
+    # ================================= plot and save  ===============================
+    # =============================================================================
+
+    timeind = (cell.tvec > np.argmax(cell.somav)*st -
+            3) & (cell.tvec <= np.argmax(cell.somav)*st+5)
+
+
+    # fileout="Vlfpy_BS"+"_LA"+LA+"_DA"+DA+"_LD"+LD+"_DD"+DD+"demo.txt"
+    # np.savetxt(fileout,meshgrid_electrodes.LFP.T[timeind])
+    Vlfpy = meshgrid_electrodes.LFP.T[timeind]
+
+    # fileout2="Vm_BS"+"_LA"+LA+"_DA"+DA+"_LD"+LD+"_DD"+DD+"demo.txt"
+    # np.savetxt(fileout2,cell.somav[timeind])
+    Vmlfpy = cell.somav[timeind]
+
+    # fileout3="Im_BS"+"_LA"+LA+"_DA"+DA+"_LD"+LD+"_DD"+DD+"demo.txt"
+    # np.savetxt(fileout3,cell.imem[0,timeind])
+    Imlfpy = cell.imem[0,timeind]
+
+    # elpos=(x_elec,y_elec,z_elec)
+    # np.savetxt("elpos_demo",elpos)
+    res = StimulationResult()
+    res.Vlfpy = Vlfpy
+    res.Vmlfpy = Vmlfpy
+    res.Imlfpy = Imlfpy
+    res.cell = cell
+    res.timeind = timeind
+    res.stimulus = stimulus
+    res.meshgrid_electrodes = meshgrid_electrodes
+    return res
+
+class StimulationResult:
+    Vlfpy = None
+    Vmlfpy = None
+    Imlfpy = None
+    cell = None
+    timeind = None
+    stimulus = None
+    meshgrid_electrodes = None
+
+def plotStimulation(cell, timeind, stimulus, meshgrid_electrodes):
+    fig = plt.figure(1)
+    # ================= STIMULATION PLOT ==========================================
+    plt.subplot(411)
+    pl.plot(cell.tvec[timeind], stimulus.i[timeind])
+    plt.axis('tight')
+    plt.ylabel(r'$I_s$ (nA)', va='center')
+    plt.grid(True)
+    # ================= SOMA PLOT =================================================
+    plt.subplot(412)
+    plt.plot(cell.tvec[timeind], cell.somav[timeind], lw=1)
+    plt.axis('tight')
+    plt.ylabel(r'$V_m$ (mV)', va='center')
+    plt.grid(True)
+
+    # ================= Imem  ============================================
+    plt.subplot(413)
+    # ,label='x='+str(elec_parameters["x"])+' - y='+str(elec_parameters["y"]))
+    plt.plot(cell.tvec[timeind], cell.imem[0, timeind], lw=1)
+    plt.axis('tight')
+    plt.ylabel(r'$I_m$ (nA)', va='center')
+    plt.grid(True)
+
+    # ================= ELECTRODE LFP  ============================================
+    plt.subplot(414)
+    # ,label='x='+str(elec_parameters["x"])+' - y='+str(elec_parameters["y"]))
+    plt.plot(cell.tvec[timeind], meshgrid_electrodes.LFP.T[timeind], lw=1)
+    plt.ylabel(r'$V_e$ (mV)', va='center')
+    plt.xlabel('time (ms)')
+    plt.grid(True)
+
+    plotstuff(cell, meshgrid_electrodes)
+    plt.show()
