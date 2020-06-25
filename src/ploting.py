c78a152d7529b00563e55d7b50b3849739f16716
Tony Zhou <tony.zhou@telecomnancy.eu>
2020-06-25 18:57:10 +0200

    Meilleure version ?

--- src/ploting.py
+++ src/ploting.py
@@ -6,6 +6,8 @@ from neuron import h
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
             
+
+            
 def plotCylinder(x0, x1, diam, ax, colour, name):
     """
     Dessine un cylindre étant donnés son origine, son sommet et son diamètre.
@@ -22,6 +24,7 @@ def plotCylinder(x0, x1, diam, ax, colour, name):
     Return:
         - array(float), array(float), array(float)
     """
+    nseg = 10
     v = x1 - x0
     mag = np.linalg.norm(v)
     v = v/mag
@@ -31,9 +34,9 @@ def plotCylinder(x0, x1, diam, ax, colour, name):
     n1 = np.cross(v, not_v)
     n1 /= np.linalg.norm(n1)
     n2 = np.cross(v, n1)
-    t = np.linspace(0, mag, 5)
-    theta = np.linspace(0, 2 * np.pi, 10)
-    rsample = np.linspace(0, diam/2, 5)
+    t = np.linspace(0, mag, nseg)
+    theta = np.linspace(0, 2 * np.pi, 2*nseg)
+    rsample = np.linspace(0, diam/2, nseg)
     t, theta2 = np.meshgrid(t, theta)
     rsample,theta = np.meshgrid(rsample, theta)
     X, Y, Z = [x0[i] + v[i] * t + diam/2 * np.sin(theta2) * n1[i] + diam/2 * np.cos(theta2) *       n2[i] for i in [0, 1, 2]]
@@ -60,11 +63,19 @@ def plot3DCell(cell):
     Return:
         - None
     """
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    fig1 = plt.figure("3D")
+    fig2 = plt.figure("2D Z")
+    fig3 = plt.figure("2D Y")
+    fig4 = plt.figure("2D X")
+    colormap = plt.get_cmap('jet')
+    nrn_col = plt.get_cmap('Spectral')
+    ax = fig1.add_subplot(111, projection='3d')
+    axXY = fig2.add_subplot(111)
+    axXZ = fig3.add_subplot(111)
+    axYZ = fig4.add_subplot(111)
     h.define_shape()
     max_range = 0
-    cmap = plt.get_cmap('Spectral')
+    x, y, z = [], [], []
     i = 0
     N = sum(1 for s in cell.allsec())
     # en attendant de trouver une méthode pour avoir le nombres de sections...
@@ -72,8 +83,11 @@ def plot3DCell(cell):
         x0 = np.array([s.x3d(0), s.y3d(0), s.z3d(0)])
         x1 = np.array([s.x3d(1), s.y3d(1), s.z3d(1)])
         d = s.diam3d(0)
-        colour = cmap(i/N)
+        colour = nrn_col(i/N)
         X, Y, Z = plotCylinder(x0, x1, d, ax, colour, s.name())
+        x.extend(list(X))
+        y.extend(list(Y))
+        z.extend(list(Z))
         max_range = np.array([X.max()-X.min(), Y.max()-Y.min(), Z.max()-Z.min(), max_range]).max()
         i+=1
         
@@ -85,11 +99,53 @@ def plot3DCell(cell):
 
     for xb, yb, zb in zip(Xb, Yb, Zb):
         ax.plot([xb], [yb], [zb], 'w')
-            
+    
+    x = np.array(x)
+    y = np.array(y)
+    z = np.array(z)
+    caXY = axXY.contour(x, y, z, np.linspace(z.min(), z.max(), 1000), cmap = colormap)
+    caXZ = axXZ.contourf(x, z, y, np.linspace(y.min(), y.max(), 150), cmap = colormap)
+    caYZ = axYZ.contourf(y, z, x, np.linspace(x.min(), x.max(), 150), cmap = colormap)
+    print(z)
+    axXY.set_xlabel("X (µm)")
+    axXY.set_ylabel("Y (µm)")
+    axXY.set_title("XY plan")
+    cbarXY = fig2.colorbar(caXY, ax = axXY)
+    cbarXY.set_label("Depth in Z axis (µm)")
+    axXY.grid()
+    
+    axXZ.set_xlabel("X (µm)")
+    axXZ.set_ylabel("Z (µm)")
+    axXZ.set_title("XZ plan")
+    cbarXZ = fig3.colorbar(caXZ, ax = axXZ)
+    cbarXZ.set_label("Depth in Y axis (µm)")
+    axXZ.grid()
+    
+    
+    axYZ.set_xlabel("Y (µm)")
+    axYZ.set_ylabel("Z (µm)")
+    axYZ.set_title("YZ plan")
+    cbarYZ = fig4.colorbar(caYZ, ax = axYZ)
+    cbarYZ.set_label("Depth in X axis (µm)")
+    axYZ.grid()
+
+
+        
     ax.set_xlabel("X (µm)")
     ax.set_ylabel("Y (µm)")
     ax.set_zlabel("Z (µm)")
-    plt.title("Model of 3D cell")
-    plt.grid()
-    plt.legend()
+    ax.set_title("Model of 3D cell")
+    
+    # make the panes transparent
+    #ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    #ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    #ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    # make the grid lines transparent
+    ax.xaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+    ax.yaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+    ax.zaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+    
+    
+    
+    ax.legend()
     plt.show()
